989b1b81e5e0aaec57ae780981afcbfe25c73676
Narin Wattanasiri <nwattana@z2t1c2.42bangkok.com>
2023-06-04 12:50:52 +0700

    ADD cpp00 Done

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 #include <string>
+#include <stdio.h>
 
-
-int main(){
-	std::string str;
-	std::cin >> str;
-	std::cout << str;
+int main(int av, char **ac)
+{
+	int j;
+	
+	if (av == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+	for (int i = 1; i<av; i++)
+	{
+		j = 0;
+		while (ac[i][j] != '\0')
+		{
+			std::cout << char(toupper(ac[i][j]));
+			j++;
+		}
+	}
+	std::cout << std::endl;
 }
