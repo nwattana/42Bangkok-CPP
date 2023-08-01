ceff85dfd3ed6bae3c2e19ffde71be18b00bba5b
narin <narin.w@lexnetix.com>
2023-08-01 19:49:23 +0000

    ADD cpp02

---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,29 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+    private:
+        // an integer to store the fixed point value
+        int _value;
+        // the number of fractional bits always be 8
+        static const int _bits = 8;
+
+    public:
+        // Default constructor
+        Fixed(void);
+        // Copy constructor
+        Fixed(const Fixed &src);
+        // Destructor
+        ~Fixed(void);
+        // Assignment operator overload
+        Fixed &operator=(const Fixed &rhs);
+    
+        int getRawBits(void) const;
+        void setRawBits(int const raw);
+};
+// TODO เขียน ยังไม่เสร็จนะ
+// แค่ Copy จาก ex00
+#endif
